bf3e8143292adf55d9a6e983e4289c64a792d3d4
Roberto Rosa <roberto.rosa7@gmail.com>
2022-06-06 15:46:08 -0300

    implementando dashboard cypress no projeto

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Cypress
+# 2372-automacao-e2e-cypress
+Projeto utilizado no curso de Introdução a automação e2e com Cypress 
---
+++ cypress/integration/3-alurapic/alurapic.spec.js
@@ -0,0 +1,32 @@
+describe('Login e Registro de usuário Alura Pic', () => {
+  beforeEach(() => {
+    cy.visit('https://alura-fotos.herokuapp.com');
+  });
+
+  it('Verificar mensagem de validação', () => {
+    cy.contains('a', 'Register now').click();
+    cy.contains('button', 'Register').click();
+    cy.contains('ap-vmessage', 'Email is required').should('be.visible');
+
+    cy.contains('button', 'Register').click();
+
+    cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
+    cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+    cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
+  });
+
+  it('Verificar mensagem de email invalido', () => {
+    cy.contains('a', 'Register now').click();
+    cy.contains('button', 'Register').click();
+    cy.get('input[formcontrolname="email"]').type('jaqueline');
+    cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+  });
+
+  it('Verificar senha com menos de 8 caracteres', () => {
+    cy.contains('a', 'Register now').click();
+    cy.contains('button', 'Register').click();
+    cy.get('input[formcontrolname="password"]').type('1234');
+    cy.contains('button', 'Register').click();
+    cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+  });
+});
