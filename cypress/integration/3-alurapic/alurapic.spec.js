ff5eb065d96717146a2a13bd252aa2013e0e622f
Roberto Rosa <roberto.rosa7@gmail.com>
2023-03-26 21:43:12 -0300

    update

--- cypress/integration/3-alurapic/alurapic.spec.js
+++ cypress/integration/3-alurapic/alurapic.spec.js
@@ -30,15 +30,28 @@ describe('Login e Registro de usuário Alura Pic', () => {
     cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
   });
 
-  it.only('Fazer login de usuário valido', () => {
+  it('Fazer login de usuário valido', () => {
     cy.login('flavio', '123');
     cy.contains('a', '(Logout)').should('be.visible');
   });
 
-  it.only('Fazer login de usuário invalido', () => {
+  it('Fazer login de usuário invalido', () => {
     cy.login('roberto', '1234');
     cy.on('window:alert', (str) => {
       expect(str).to.equal('Invalid user name or password');
+    });
+  });
+
+  const usuarios = require('../../fixtures/usuarios.json');
+  usuarios.forEach((user) => {
+    it.only(`Fazer novo Registro de usuário ${user.userName}`, () => {
+      cy.contains('a', 'Register now').click();
+      cy.contains('button', 'Register').click();
+      cy.get('input[formcontrolname="email"]').type(user.email);
+      cy.get('input[formcontrolname="fullName"]').type(user.fullName);
+      cy.get('input[formcontrolname="userName"]').type(user.userName);
+      cy.get('input[formcontrolname="password"]').type(user.password);
+      cy.contains('button', 'Register').click();
     });
   });
 });
